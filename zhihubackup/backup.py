949584a61d0219aa3e625081661b1dd398874a8c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-08-29 14:44:36 -0400

    debug cookie

--- zhihubackup/backup.py
+++ zhihubackup/backup.py
@@ -33,6 +33,7 @@ def get_cookie(username):
 
 
 def get_json(url, cookie):
+    print(cookie)
     d_c0 = cookie.split("d_c0=")[1].split(";")[0]
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75",
